d007069277013b75c5f3b67cb884c034a733cbdd
roland <roland.erkiert@gmail.com>
2021-01-30 08:09:21 +0100

    Updates

---
+++ precache-manifest.d0c6997e29b82e22404038f8a1fbe188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3876819b312a2918422bd47069025a98",
+    "url": "/cartelgame/index.html"
+  },
+  {
+    "revision": "1ace7cf6772393459e92",
+    "url": "/cartelgame/static/css/main.f09ac323.chunk.css"
+  },
+  {
+    "revision": "8372485b09ef06cca80d",
+    "url": "/cartelgame/static/js/2.1401583f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cartelgame/static/js/2.1401583f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ace7cf6772393459e92",
+    "url": "/cartelgame/static/js/main.52d63b35.chunk.js"
+  },
+  {
+    "revision": "ef4920933bd5bdfce06b",
+    "url": "/cartelgame/static/js/runtime-main.d200caea.js"
+  }
+]);
